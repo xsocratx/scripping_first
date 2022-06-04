5df9422c57e14bfd023a2100b5ca36601f94a27c
@SiarheiShumik <siarhei.shumik@group-itc.com>
2022-06-04 18:34:45 +0200

    scrapper2

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 import imp
+from turtle import ht
 from urllib.request import urlopen
 
 url = "http://olympus.realpython.org/profiles/aphrodite"
@@ -9,3 +10,17 @@ print(page)
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
+
+print(html)
+
+title_index = html.find("<title>")
+
+print(title_index)
+
+start_index = title_index + len("<title>")
+
+end_index = html.find("</title>")
+
+title = html[start_index:end_index]
+
+print(title)
---
+++ scrapper2.py
@@ -0,0 +1,16 @@
+from re import U
+from urllib.request import urlopen
+
+
+url = "http://olympus.realpython.org/profiles/poseidon"
+
+page = urlopen(url)
+
+html = page.read().decode("utf-8")
+
+start_index = html.find("<title>") + len("<title>")
+end_index = html.find("</title>")
+
+title = html[start_index:end_index]
+
+print(title)
